bf3edf8dc4bf6837b9105cf228b385b8c568c240
Kamil Poncyliusz <kamil.poncyliusz@gmail.com>
2024-05-15 18:23:29 +0200

    Add a select element to choose a map variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import "./App.css";
-// import PerlinNoise from "./components/PerlinNoise";
+import CellularAutomataMap from "./components/CellularAutomataMap";
+import PerlinNoise from "./components/PerlinNoise";
 import PerlinNoiseMap from "./components/PerlinNoiseMap";
+import { VariantSelectField } from "./components/VariantSelectField";
 
 function App() {
+  const [mapVariant, setMapVariant] = useState(0);
+  function EmptyVariant() {
+    return (
+      <div id="generator-wrapper">
+      <div id="controls-window"></div>
+      <div id="map-preview-window"></div>
+    </div>
+    )
+  }
+  const mapVariantComponents = [
+    EmptyVariant,
+    PerlinNoiseMap,
+    PerlinNoise,
+    CellularAutomataMap,
+  ];
+  const changeMapVariant = (newValue: number) => {
+    if(newValue < 0 || newValue >= mapVariantComponents.length) return;
+    setMapVariant(newValue);
+  };
+  const SelectedComponent = mapVariantComponents[mapVariant];
   return (
-    <PerlinNoiseMap />
+    <>
+      <VariantSelectField changeHandler={changeMapVariant}/>
+      {<SelectedComponent />}
+    </>
   );
 }
 
---
+++ src/components/VariantSelectField.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export function VariantSelectField({changeHandler}: {changeHandler: (newValue: number) => void}){
+  const [selectedValue, setSelectedValue] = useState("0");
+  function handleChange(e: React.ChangeEvent<HTMLSelectElement>){
+    const target = e.target as HTMLSelectElement;
+    setSelectedValue(target.value);
+    changeHandler(parseInt(target.value));
+  }
+  
+  return (
+    <div id="variant-selection-wrapper">
+      <select name="variant-select" id="variant-select" value={selectedValue} onChange={handleChange}>
+        <option value="0" disabled>Select a variant</option>
+        <option value="1">Perlin Noise Map</option>
+        <option value="2">Perlin Noise</option>
+        <option value="3">Cellular Automata Map</option>
+      </select>
+    </div>
+  );
+}
